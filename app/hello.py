5cef8ffb69d99144172e1df132b87a19cefd2df7
ashish__narang <ashish.narang@hcode.tech>
2022-05-30 09:23:09 +0530

    linting test

---
+++ app/hello.py
@@ -0,0 +1,18 @@
+from tests.integration_tests.utils import (
+    create_model_with_movielens_dataset_and_assert_status,
+)
+
+x = "ashdajkdoiewjdsncndskhfsdchdsfyusdhjdskjlkasoudisajdhjnmskldsfgysjdfiusofupolksdfjkfdsbsudgfbnsdbvcsjkdhsufyhjdsnbsfhdsfgsjfsiugfhdsfdhgiuodhgsiudhgsiudshoiehjoifdijdkkjjhjlksjfdsfysiudfbsgdgdyufdkjdshkfskhdfkhskhfksdfhsjdfsfsjfsdjfhuskashishsiahsiahsihahsiahsiahsihahsa"
+create_model_with_movielens_dataset_and_assert_status(
+    create_model_with_movielens_dataset_and_assert_status(
+        create_model_with_movielens_dataset_and_assert_status(
+            create_model_with_movielens_dataset_and_assert_status(
+                create_model_with_movielens_dataset_and_assert_status(
+                    create_model_with_movielens_dataset_and_assert_status(
+                        create_model_with_movielens_dataset_and_assert_status
+                    )
+                )
+            )
+        )
+    )
+)
